68fc41a388ff73b224442e6967bfdd7d015c2171
Aguppesce <aguppesce@gmail.com>
2022-09-10 21:30:32 -0300

    Ejercicio 04

--- 00.JavaIntro/Java00_Intro/src/Java04_Ejercicio.java
+++ 00.JavaIntro/Java00_Intro/src/Java04_Ejercicio.java
@@ -1,24 +1,22 @@
 /**
- *
- * @Agu 
- */
-package JavaIntro;
+ * @autor Aguppesce
+ * */
+
+//Ejercicio 03: Escribir un programa que pida tu nombre, lo guarde en una variable y lo muestre por pantalla.
 
 import java.util.Scanner;
 
-public class Ejercicio03 {
-
+public class Java03_Ejercicio {
     public static void main(String[] args) {
         String var,nom;
-        
+
         Scanner leer=new Scanner(System.in);
-        
+
         System.out.print("Ingrese su nombre: ");
         nom = leer.nextLine();
         var=nom;
         System.out.print("Su nombre es: "+var+"\n");
-        
-        
+
+
     }
-    
 }
