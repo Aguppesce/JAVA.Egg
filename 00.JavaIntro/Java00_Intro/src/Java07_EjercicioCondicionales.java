2efc8138145082b856b60fa48c77f10818e8674f
Aguppesce <aguppesce@gmail.com>
2022-09-10 21:36:07 -0300

    Ejercicio 07 - Condicionales

--- 00.JavaIntro/Java00_Intro/src/Java07_EjercicioCondicionales.java
+++ 00.JavaIntro/Java00_Intro/src/Java07_EjercicioCondicionales.java
@@ -1,28 +1,26 @@
 /**
- *
- * @Agu 
- */
-package JavaIntro;
+ * @autor Aguppesce
+ * */
+
+//Ejercicio 06: Escribir un programa que lea un número entero por teclado y muestre por pantalla el doble, el triple y la raíz cuadrada de ese número. Nota: investigar la función Math.sqrt().
 
 import java.util.Scanner;
 
-public class Ejercicio07 {
-
+public class Java07_Ejercicio {
     public static void main(String[] args) {
         int num1,num2;
-        
+
         Scanner leer=new Scanner(System.in);
-        
+
         System.out.print("Ingrese número 1: ");
         num1 = leer.nextInt();
         System.out.print("Ingrese número 2: ");
         num2 = leer.nextInt();
-       
+
         if(num1>num2){
             System.out.print(num1+" es el mayor \n");
         }else{
             System.out.print(num2+" es el mayor \n");
-        }   
-    }   
+        }
+    }
 }
-
