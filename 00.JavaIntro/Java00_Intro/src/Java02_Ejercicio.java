f025ddff162242ec827036cd3a9e608a7a9f0eaa
Aguppesce <aguppesce@gmail.com>
2022-09-10 21:23:55 -0300

    Ejercicios Intro 01 a 03

---
+++ 00.JavaIntro/Java00_Intro/src/Java02_Ejercicio.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Java01_Ejercicio {
+    public static void main(String[] args) {
+        int num1,num2;
+
+        Scanner leer=new Scanner(System.in);
+
+        System.out.print("Ingrese número 1: ");
+        num1 = leer.nextInt();
+        System.out.print("Ingrese número 2: ");
+        num2 = leer.nextInt();
+        System.out.print("La suma es: "+(num1+num2)+"\n");
+    }
+}
--- 00.JavaIntro/Java00_Intro/src/Java03_Ejercicio.java
+++ 00.JavaIntro/Java00_Intro/src/Java03_Ejercicio.java
@@ -1,25 +1,17 @@
-/**
- *
- * @Agu 
- */
-package JavaIntro;
+//Ejercicio 02: Escribir un programa que pida dos números enteros por teclado y calcule la suma de los dos. El programa deberá después mostrar el resultado de la suma
 
 import java.util.Scanner;
 
-public class Ejercicio02 {
-
+public class Java02_Ejercicio {
     public static void main(String[] args) {
         int num1,num2;
-        
+
         Scanner leer=new Scanner(System.in);
-        
+
         System.out.print("Ingrese número 1: ");
         num1 = leer.nextInt();
         System.out.print("Ingrese número 2: ");
         num2 = leer.nextInt();
         System.out.print("La suma es: "+(num1+num2)+"\n");
-        
-        
     }
-    
 }
